4db5abda853d1dd018ffcf1c123ffe5bfe5d5510
Snuger <explemsnii@outlook.com>
2018-10-18 22:32:26 +0800

    TPL DataFlow

--- Parallel/ParallelSample/DataFlowSample/Program.cs
+++ Parallel/ParallelSample/DataFlowSample/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
+using System.IO;
+using System.Collections.Generic;
 
 namespace DataFlowSample
 {
@@ -36,9 +38,13 @@ namespace DataFlowSample
 
         static void Main(string[] args) {
 
-            Task t1 = Task.Run(()=>Producter());
-            Task t2 = Task.Run(async()=> await ConsumerAsync());
-            Task.WaitAll(t1,t2);
+            // Task t1 = Task.Run(()=>Producter());
+            // Task t2 = Task.Run(async()=> await ConsumerAsync());
+            //Task.WaitAll(t1,t2);
+
+            var target = setupPipeline();
+            target.Post(@"E:\Code Studio\C#-Project\Cshartp6ProfessionalSample\Parallel\ParallelSample\DataFlowSample");
+            Console.ReadLine();
         }
 
 
@@ -62,6 +68,69 @@ namespace DataFlowSample
                 string data = await s_buffer.ReceiveAsync();
                 Console.WriteLine($"user input data:{data}");
             }
+        }
+
+
+
+        private static IEnumerable<string> GetFileNames(string path) {
+            foreach (var fileName in Directory.EnumerateFiles(path, "*.cs")) {
+                yield return fileName;
+            }
+        }
+
+        private static IEnumerable<string> LoadLines(IEnumerable<string> fileNames) {
+            foreach (var fileName in fileNames) {
+                using (FileStream stream = File.OpenRead(fileName)) {
+                    var reader = new StreamReader(stream);
+                    string line = string.Empty;
+                    while ((line=reader.ReadLine()) != null) {
+                        yield return line;
+                    }
+                }
+
+            }
+
+        }
+
+        private static IEnumerable<string> GetWords(IEnumerable<string> lines) {
+
+            foreach (string line in lines) {
+                string[] words = line.Split(' ', ';', '(', ')', '{', '}', '.', ',');
+                foreach (string word in words)
+                {
+                    if (!string.IsNullOrEmpty(word))                    
+                        yield return word;                   
+
+                }
+            }
+        }
+
+        private static ITargetBlock<string> setupPipeline() {
+            var fileNamesForPath = new TransformBlock<string, IEnumerable<string>>(path=> {
+                return GetFileNames(path);
+            });
+
+
+            var lines = new TransformBlock<IEnumerable<string>, IEnumerable<string>>(fileNames=> {
+                 return LoadLines(fileNames);
+            });
+
+
+           // var words = new TransformBlock<IEnumerable<string>, IEnumerable<string>>(lines1=> {
+           //      return GetWords(lines1);
+           // });
+
+            var dispplay = new ActionBlock<IEnumerable<string>>(lines2=> {
+                foreach (string s in lines2) {
+                    Console.WriteLine(s);
+                }
+            });
+
+            fileNamesForPath.LinkTo(lines);
+            lines.LinkTo(dispplay);
+           // lines.LinkTo(words);
+           // words.LinkTo(dispplay);
+            return fileNamesForPath;
         }
     }
 }
