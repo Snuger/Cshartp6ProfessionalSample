a19e6e27a0cf7e78485d43340c07508c888ec854
Snuger <explemsnii@outlook.com>
2019-04-28 17:46:48 +0800

    代码同步

--- WinForm/Stylet/StyletSamples/View/ListBoxSample.xaml.cs
+++ WinForm/Stylet/StyletSamples/View/ListBoxSample.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StyletSamples.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,8 @@ namespace StyletSamples.View
     {
         public ListBoxSample()
         {
-            InitializeComponent();    
+            InitializeComponent();
+            this.DataContext = new ListBoxSampleViewModel();
         }   
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
--- WinForm/Stylet/StyletSamples/ViewModel/ListBoxSampleViewModel.cs
+++ WinForm/Stylet/StyletSamples/ViewModel/ListBoxSampleViewModel.cs
@@ -46,6 +46,5 @@ namespace StyletSamples.ViewModel
             CurrentSelect = listbox.SelectedItem.ToString();          
         }
 
-
     }
 }
