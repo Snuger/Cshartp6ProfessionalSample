1c4aee1a0a9b268b6088777c9299cb6a8ad87aca
Snuger <explemsnii@outlook.com>
2019-10-28 23:19:47 +0800

    add rabbitmq samples

---
+++ RabbitMQ/Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "10.75.0.1"
+            };
+            var connect = factory.CreateConnection();
+            var channel = connect.CreateModel();
+            channel.QueueDeclare("hello", false, false, false, null);
+            System.Console.WriteLine("队列管理器已经成功连接上，请输入消息，或者输入exit退出");
+            string input = string.Empty;
+            do
+            {
+                input = Console.ReadLine();
+                var sendBytes = Encoding.UTF8.GetBytes(input);
+                channel.BasicPublish("", "hello", null, sendBytes);
+
+            } while (input.Trim().ToLower() != "exit");
+            channel.Close();
+            connect.Close();
+        }
+    }
+}
