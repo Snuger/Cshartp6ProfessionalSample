d6309d61bfc7e2fc6771d8e70ca9e301b01f5cad
Snuger <explemsnii@outlook.com>
2020-01-16 22:06:59 +0800

    netcore配置文件读取与自定义配置文件的使用

---
+++ Core/HostConfigs/HostConfigSamples/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HostConfigSamples.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        private readonly MyOptions _option;
+
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IOptions<MyOptions> option)
+        {
+            _logger = logger;
+            _option = option.Value;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("/{id}")]
+        public ActionResult GetVersion(string id)
+        {
+            return Ok(new { code = 1, data = new { name = _option.Name, Version = _option.Version } });
+        }
+    }
+}
