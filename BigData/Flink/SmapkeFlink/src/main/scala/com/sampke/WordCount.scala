70fa44e266d46186a04a2bdbbd1ef2ae19ba5025
snuger <explemsnii@outlook.com>
2020-11-15 21:51:23 +0800

    flink的项目基本环境

---
+++ BigData/Flink/SmapkeFlink/src/main/scala/com/sampke/WordCount.scala
@@ -0,0 +1,10 @@
+package com.sampke
+
+import  org.apache.flink.streaming.api.scala._
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+     val env=StreamExecutionEnvironment.getExecutionEnvironment
+      //var intputDataStream=env.socketTextStream()
+  }
+}
