ce462e73238b0356bba7bf03ccf257f8181bddb4
snuger <explemsnii@outlook.com>
2022-02-27 17:05:28 +0800

    移除文件

--- Basic/LinqSamples/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LinqSamples
-{
-    internal class Product
-    {
-        public string Category { get; internal set; }
-        public int Id { get; internal set; }
-        public double Value { get; internal set; }
-    }
-}
